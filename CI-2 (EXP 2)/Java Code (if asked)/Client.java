3850390943e1dd43ffa63f1707d15e7eb75eb595
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-05-04 14:44:37 +0530

    exp 2 updated

--- CI-2 (EXP 2)/Java Code (if asked)/Client.java
+++ CI-2 (EXP 2)/Java Code (if asked)/Client.java
@@ -1,8 +1,16 @@
 import java.rmi.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         Concat stub = (Concat) Naming.lookup("rmi://localhost/concat");
-        System.out.println("Result: " + stub.concat("Hi", "there!"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = scanner.nextLine();
+        String result = stub.concat(str1, str2);
+        System.out.println("Result: " + result);
+        scanner.close();
     }
 }
