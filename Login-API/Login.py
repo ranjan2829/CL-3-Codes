57b19bf82f6fb517c70eea7d9261f59008fa5a48
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-06-25 15:45:52 +0530

    done

--- Login-API/Login.py
+++ Login-API/Login.py
@@ -1,16 +1,13 @@
 import os
 from dotenv import load_dotenv
 from fyers_apiv3 import fyersModel
-
 load_dotenv()
-
 session = fyersModel.SessionModel(
     client_id=os.getenv("client_id"),
     secret_key=os.getenv("secret_key"),
     redirect_uri="https://trade.fyers.in/api-login/redirect-uri/index.html",
     response_type="code"
 )
-
 response = session.generate_authcode()
 print(response)
 
