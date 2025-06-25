b0b6b635bab2691cc9fbb551af4a5320e90b946d
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-06-25 15:45:26 +0530

    done

--- Login-API/Login.py
+++ Login-API/Login.py
@@ -1,15 +1,16 @@
+import os
+from dotenv import load_dotenv
 from fyers_apiv3 import fyersModel
-client_id = "QGP6MO6UJQ-100"
-secret_key = "2X22I3160A"
-redirect_uri = "https://trade.fyers.in/api-login/redirect-uri/index.html"
-response_type = "code"  
-state = "sample_state"
+
+load_dotenv()
+
 session = fyersModel.SessionModel(
-    client_id=client_id,
-    secret_key=secret_key,
-    redirect_uri=redirect_uri,
-    response_type=response_type
+    client_id=os.getenv("client_id"),
+    secret_key=os.getenv("secret_key"),
+    redirect_uri="https://trade.fyers.in/api-login/redirect-uri/index.html",
+    response_type="code"
 )
+
 response = session.generate_authcode()
 print(response)
 
--- Login-API/access_token.py
+++ Login-API/access_token.py
@@ -1,21 +1,14 @@
+import os
+from dotenv import load_dotenv
 from fyers_apiv3 import fyersModel
-client_id = "QGP6MO6UJQ-100"
-secret_key = "2X22I3160A"
-redirect_uri = "https://trade.fyers.in/api-login/redirect-uri/index.html"
-response_type = "code"  
-state = "sample_state"
-grant_type = "authorization_code"  
-
-auth_code = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJRR1A2TU82VUpRIiwidXVpZCI6ImY3N2FkMjM5ZGIzODRmNDk4ZmVjNWYwYmNkNjJlMTExIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhSMjAxODUiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJkZWNhY2RhZDNmNzdjMGNkYTE0OThlNzY1MzdiMTMyYjcxNGMyZTg0NmQzNDFmMmZiYzkzZmY1YSIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiLFwieDoyXCJdIiwiZXhwIjoxNzQ4OTUyODI1LCJpYXQiOjE3NDg5MjI4MjUsImlzcyI6ImFwaS5sb2dpbi5meWVycy5pbiIsIm5iZiI6MTc0ODkyMjgyNSwic3ViIjoiYXV0aF9jb2RlIn0.2nM0K21OilE9jfSm8yDRORyhF1gG7zM2SMp8TIfb4D0"
-
+load_dotenv()
 session = fyersModel.SessionModel(
-    client_id=client_id,
-    secret_key=secret_key, 
-    redirect_uri=redirect_uri, 
-    response_type=response_type, 
-    grant_type=grant_type
-)
-session.set_token(auth_code)
+    client_id=os.getenv("client_id"),
+    secret_key=os.getenv("secret_key"), 
+    redirect_uri="https://trade.fyers.in/api-login/redirect-uri/index.html",
+    response_type="code", 
+    grant_type="authorization_code")
+session.set_token(os.getenv("auth_code"))
 response = session.generate_token()
 print(response)
 
